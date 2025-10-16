ce5c5f855285c7fe506ccb15a9bce7aa1733926d
Vinícius <vinicius@email.com>
2025-10-16 11:34:24 -0300

    add morgan to log requests

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import routes from './routes';
 import { setupSwagger } from './config/swagger';
 import { routeNotFound } from './config/middleware/route-not-found.middleware';
 import { errorHandler } from './config/middleware/error-handler.middleware';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ const app = express();
 // if needed change to body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
 
 app.use(express.static('public'));
 
